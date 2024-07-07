93027427e1f0adf38407ea0c7232568dc37451e1
dguy1470 <142624252+dguy1470@users.noreply.github.com>
2024-07-07 10:13:00 -0400

    Create RPG_stats.py

---
+++ RPG_stats.py
@@ -0,0 +1,25 @@
+
+#defines our stats
+class Stats:
+    def __init__(self,HP,Mana,AD,MD,Def,MR,MS,Reg,Mreg,CD,Crit,Gou,Pen,STA,LS,Ten,AS,EN):
+        self.HP = float(HP)
+        self.Mana = float(Mana)
+        self.AD = float(AD)
+        self.MD = float(MD)
+        self.Def = float(Def)
+        self.MR = float(MR)
+        self.MS = float(MS)
+        
+        self.Reg = float(Reg)
+        self.Mreg = float(Mreg)
+        self.CD = float(CD)
+        self.Crit = float(Crit)
+        self.Gou = float(Gou)
+        self.Pen = float(Pen)
+        
+        self.STA = float(STA)
+        self.LS = float(LS)
+        self.Ten = float(Ten)
+        self.AS = float(AS)
+        self.EN = float(EN)
+
